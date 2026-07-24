b2c8f432f7a7c4abf6d629d6a4a4fde1e2b3fdc4
Dreeper160 <142353112+Dreeper160@users.noreply.github.com>
2026-07-25 01:57:06 +0200

    Enhance music server with upload functionality

--- play/startserver.lua
+++ play/startserver.lua
@@ -8,10 +8,9 @@ local decoder = dfpwm.make_decoder()
 print("Serveur Jukebox en ligne ! En attente de requêtes...")
 
 while true do
-    -- Attente d'un message réseau par ondes radio
     local id_joueur, message = rednet.receive()
     
-    -- Si le joueur demande la liste des musiques
+    -- 1. Le joueur demande la liste
     if type(message) == "table" and message.action == "list" then
         local fichiers = fs.list("disk")
         local musiques = {}
@@ -20,20 +19,16 @@ while true do
         end
         rednet.send(id_joueur, musiques)
         
-    -- Si le joueur demande de jouer une musique
+    -- 2. Le joueur lance une musique
     elseif type(message) == "table" and message.action == "play" then
-        print("Demande reçue : " .. message.chanson .. " pour " .. message.enceinte)
-        
+        print("Demande de lecture : " .. message.chanson)
         local file = fs.open("disk/" .. message.chanson, "rb")
         if file then
-            rednet.send(id_joueur, "Lecture en cours dans la chambre...")
-            
+            rednet.send(id_joueur, "Lecture en cours...")
             while true do
                 local chunk = file.read(16 * 1024)
                 if not chunk then break end
                 local buffer = decoder(chunk)
-                
-                -- Envoie l'audio dans les câbles (Wired Modem) jusqu'à l'enceinte
                 while not peripheral.call(message.enceinte, "playAudio", buffer) do
                     os.pullEvent("speaker_audio_empty")
                 end
@@ -41,7 +36,31 @@ while true do
             file.close()
             print("Lecture terminée.")
         else
-            rednet.send(id_joueur, "Erreur : Fichier introuvable sur le disque du serveur.")
+            rednet.send(id_joueur, "Erreur : Fichier introuvable.")
+        end
+
+    -- 3. NOUVEAU : Le PC Uploader demande d'ajouter une musique
+    elseif type(message) == "table" and message.action == "upload" then
+        print("Téléchargement demandé : " .. message.filename)
+        rednet.send(id_joueur, "Le serveur commence le téléchargement...")
+        
+        -- On télécharge le fichier en mode binaire (true)
+        local request = http.get(message.url, nil, true)
+        
+        if request then
+            local content = request.readAll()
+            request.close()
+            
+            -- On sauvegarde directement sur la disquette
+            local file = fs.open("disk/" .. message.filename, "wb")
+            file.write(content)
+            file.close()
+            
+            print("Sauvegarde réussie : " .. message.filename)
+            rednet.send(id_joueur, "Succès : La musique a été ajoutée à la disquette !")
+        else
+            print("Échec du téléchargement.")
+            rednet.send(id_joueur, "Erreur : Le serveur n'a pas pu télécharger ce lien.")
         end
     end
 end
